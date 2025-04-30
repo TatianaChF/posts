561808093cd253ee69c8016cee80c99c205ecccd
TatianaChF <tatiana.chefox@gmail.com>
2025-04-30 12:42:50 +0300

    delete router

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<script setup lang="ts">
-import PostsPage from "./pages/PostsPage.vue";
-</script>
-
 <template>
   <PostsPage />
 </template>
+
+<script setup lang="ts">
+import PostsPage from "./pages/PostsPage.vue";
+</script>
