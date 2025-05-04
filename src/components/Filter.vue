1a7c66993a2517bc7a8b36c2e1e950b0f2c817ae
TatianaChF <tatiana.chefox@gmail.com>
2025-05-04 09:18:11 +0300

    adding filter component

---
+++ src/components/Filter.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="search-component mb-4">
+    <div class="input-group">
+      <span class="input-group-text">
+        <i class="bi bi-search"></i>
+      </span>
+      <input
+          type="text"
+          class="form-control"
+          placeholder="Search posts..."
+          aria-label="Search posts"
+      >
+      <button
+          class="btn btn-outline-secondary"
+          type="button"
+      >
+        Clear
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
